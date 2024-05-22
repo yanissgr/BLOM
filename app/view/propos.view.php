68f3abc7db4bd15c4e7cb0b265b145862618b3be
LucasPIEDADE05 <144891436+LucasPIEDADE05@users.noreply.github.com>
2024-05-22 15:35:06 +0200

    lucas

--- app/view/propos.view.php
+++ app/view/propos.view.php
@@ -1,16 +1,15 @@
 <main>
-    <div class="header">
-        <h1>BLOM: La bière artisanale belge</h1>
+<body>
+  <div class="container">
+    <div class="left">
+      <p>L'éclat de saveur</p>
     </div>
-    <div class="content">
-        <div class="text-left">
-            <p class="highlight">L'éclat de saveur</p>
-        </div>
-        <div class="text-right">
-            <p>BLOM est une marque indépendante de bière traditionnelle d'origine belge.</p>
-            <p>Nous cherchons à nous présenter de manière calme et subtile, pour nous rapprocher des saisons qui sont le thème principal de notre marque.</p>
-            <p>Le goût unique de BLOM vient du mélange de différentes saveurs distinguées telles que la fleur de cannelle, la fleur de sureau, ou même le poivre blanc.</p>
-        </div>
+    <div class="right">
+      <p>BLOM est une marque indépendante de bière traditionnelle d'origine belge.</p>
+      <p>Nous cherchons à nous présenter de manière calme et subtile, pour nous rapprocher des saisons qui sont le thème principal de notre marque.</p>
+      <p>Le goût unique de BLOM vient du mélange de différentes saveurs distinguées telles que la fleur de cannelle, la fleur de sureau, ou même le poivre blanc.</p>
     </div>
-    <hr>
+  </div>
+</body>
+</html>
 </main>
