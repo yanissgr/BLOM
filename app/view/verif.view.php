f0d860da6bd144bbff344660d9a2a122239dd9f7
yanissgr <yanis.singer91@gmail.com>
2024-05-30 10:12:43 +0200

    footer

--- app/view/verif.view.php
+++ app/view/verif.view.php
@@ -1,6 +1,7 @@
 <main>
 
     <div class="container">
+        <img src="/public/images/header/Logo_Blom.svg" alt="">
         <h2>Avez vous plus de 18 ans ?</h2>
         <button>OUI</button>
         <button>NON</button>
