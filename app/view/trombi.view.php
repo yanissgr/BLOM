06b6765b4919a80db7c528d9eb29dad00efb4000
LucasPIEDADE05 <144891436+LucasPIEDADE05@users.noreply.github.com>
2024-06-03 17:00:01 +0200

    ahhahaahahha

--- app/view/trombi.view.php
+++ app/view/trombi.view.php
@@ -1,40 +1,40 @@
 <main>
-  <div class="case">
+  <div class="case1">
     <b>Valentin</b>
     <br>18 ans
     <br>Chef de groupe MMI
   </div>
-  <div class="case">
+  <div class="case2">
     <b>Yanis</b>
     <br>18 ans
     <br>Web designer, Graphiste et Monteur
   </div>
-  <div class="case">
+  <div class="case3">
     <b>Sami</b>
     <br>19 ans
     <br>Monteur
   </div>
-  <div class="case">
+  <div class="case4">
     <b>Lucas</b>
     <br>18 ans
     <br>Community Manager
   </div>
-  <div class="case">
+  <div class="case5">
     <b>Johanne</b>
     <br>19 ans
     <br>Chef de groupe GB
   </div>
-  <div class="case">
+  <div class="case5">
     <b>Adrien</b>
     <br>18 ans
     <br>Concepteur de bière
   </div>
-  <div class="case">
+  <div class="case5">
     <b>Titouan</b>
     <br>18 ans
     <br>Concepteur de bière
   </div>
-  <div class="case">
+  <div class="case5">
     <b>Doriann</b>
     <br>18 ans
     <br>Concepteur de bière
