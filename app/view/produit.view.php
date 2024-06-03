8f521db56a2abf07c3fe2ac9564f7dafe7793cdf
yanissgr <yanis.singer91@gmail.com>
2024-06-03 16:17:31 +0200

    jsp

--- app/view/produit.view.php
+++ app/view/produit.view.php
@@ -1,18 +1,18 @@
 <main>
     <?php
     $categories = array(
-        0 => array('nom' => 'Nos offres', 'image' => 'reduction.png', 'lien' =>'liste.php?barre_recherche=Reduction'),
-        1 => array('nom' => 'Nos lots', 'image' => 'pack.png', 'lien' =>'liste.php?barre_recherche=Pack'),
-        2 => array('nom' => 'Produits individuels', 'image' => 'unique.png', 'lien' =>'liste.php?barre_recherche=Simple'),
-        3 => array('nom' => 'Points BLOM', 'image' => 'points.png', 'lien' =>'liste.php?barre_recherche=Point')
+        0 => array('nom' => 'Nos offres', 'image' => 'reduc.svg', 'lien' => 'liste.php?barre_recherche=Reduction', 'id' => 'offres'),
+        1 => array('nom' => 'Nos lots', 'image' => '4saisons.png', 'lien' => 'liste.php?barre_recherche=Pack', 'id' => 'lots'),
+        2 => array('nom' => 'Catalogue', 'image' => 'unique.png', 'lien' => 'liste.php?barre_recherche=Simple', 'id' => 'unique'),
+        3 => array('nom' => 'Points BLOM', 'image' => 'points.svg', 'lien' => 'liste.php?barre_recherche=Point', 'id' => 'points')
     );
     for ($i = 0; $i < 4; $i++) : ?>
         <a href="<?= ($categories[$i]['lien']) ?>">
             <div class=Categorie>
                 <p><?= ($categories[$i]['nom']) ?></p>
-                <div class=images>
-                    <img src="public/images/produit/<?= $categories[$i]['image'] ?>" alt="<?= $categories[$i]['nom'] ?>">
-                </div>
+                <div class="line"></div>
+                <img id='<?= ($categories[$i]['id']) ?>' src="public/images/produit/<?= $categories[$i]['image'] ?>" alt="<?= $categories[$i]['nom'] ?>">
+
             </div>
         </a>
     <?php endfor ?>
